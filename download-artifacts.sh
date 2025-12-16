6d0731a3a06ca97fe02e5fce7c251a0b5dc8b36d
Chaoyi Chen <chaoyi.chen@rock-chips.com>
2025-12-16 16:50:08 +0800

    Add support for Github CI
    Signed-off-by: Chaoyi Chen <chaoyi.chen@rock-chips.com>

--- download-artifacts.sh
+++ download-artifacts.sh
@@ -3,6 +3,10 @@
 # Downloads the latest artifacts from Collabora GitLab for the image build
 
 BOARD=$1
+DOWNLOAD_LINUX_REPO=windowsair/rk3588-linux
+DOWNLOAD_LINUX_BRANCH=rk-v6.15
+DOWNLOAD_LINUX_CI_NAME=ci.yml
+DOWNLOAD_LINUX_ARTIFACT_NAME=build-arm64-debian-package
 
 if [ -z "${BOARD}" ] ; then
     echo "ERROR: provide boardname as first argument"
@@ -15,13 +19,16 @@ if [ -d "prebuilt" ] ; then
     exit 1
 fi
 
-# Download linux artifacts for rockchip-release and rockchip-devel branches
+if ! gh auth status >/dev/null 2>&1; then
+  echo "You are not logged into any GitHub hosts. To log in, run: gh auth login"
+  exit 1
+fi
+
+# Download linux artifacts
 mkdir -p prebuilt/linux
-wget -O prebuilt/linux/linux.zip "https://gitlab.collabora.com/hardware-enablement/rockchip-3588/linux/-/jobs/artifacts/rockchip-release/download?job=build%20arm64%20debian%20package"
-unzip -j prebuilt/linux/linux.zip -d prebuilt/linux/
-wget -O prebuilt/linux/linux.zip "https://gitlab.collabora.com/hardware-enablement/rockchip-3588/linux/-/jobs/artifacts/rockchip-devel/download?job=build%20arm64%20debian%20package"
-unzip -j prebuilt/linux/linux.zip -d prebuilt/linux/
-rm prebuilt/linux/linux.zip
+LINUX_RUN_ID=$(gh run list -R "$DOWNLOAD_LINUX_REPO" -w "$DOWNLOAD_LINUX_CI_NAME" --branch "$DOWNLOAD_LINUX_BRANCH" \
+    -L 1 --status success --json databaseId -q '.[0].databaseId')
+gh run download "$LINUX_RUN_ID" -R "$DOWNLOAD_LINUX_REPO" -n "$DOWNLOAD_LINUX_ARTIFACT_NAME" -D prebuilt/linux
 
 # Download u-boot artifacts for the board
 mkdir -p prebuilt/u-boot-${BOARD}/
